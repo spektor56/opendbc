ba2c5e4e9afe2a34d8169a104706128ad30f0119
Brett Sanderson <l-brett-l@hotmail.com>
2025-07-04 00:44:54 -0400

    hyundai safety

--- opendbc/safety/modes/hyundai_canfd.h
+++ opendbc/safety/modes/hyundai_canfd.h
@@ -82,18 +82,30 @@ static void hyundai_canfd_rx_hook(const CANPacket_t *to_push) {
       update_sample(&torque_driver, torque_driver_new);
     }
 
-    // cruise buttons
+    // cruise buttons 426 and 463
     const int button_addr = hyundai_canfd_alt_buttons ? 0x1aa : 0x1cf;
     if (addr == button_addr) {
       bool main_button = false;
+      bool lkas_enabled = false;
       int cruise_button = 0;
+
       if (addr == 0x1cf) {
         cruise_button = GET_BYTE(to_push, 2) & 0x7U;
         main_button = GET_BIT(to_push, 19U);
+        lkas_enabled = GET_BIT(to_push, 23U);
       } else {
         cruise_button = (GET_BYTE(to_push, 4) >> 4) & 0x7U;
         main_button = GET_BIT(to_push, 34U);
+        lkas_enabled = GET_BIT(to_push, 39U);
       }
+
+      if ((alternative_experience & ALT_EXP_SPLIT_LKAS_AND_ACC) != 0){
+          if (lkas_enabled != lkas_enabled_prev) {
+            controls_allowed = true;
+          }
+      }
+
+      lkas_enabled_prev = lkas_enabled;
       hyundai_common_cruise_buttons_check(cruise_button, main_button);
     }
 
