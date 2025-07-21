f3ea514a03f09d8687b047ad82c6bf5ffc54fd4b
Brett Sanderson <l-brett-l@hotmail.com>
2025-07-21 00:40:44 -0400

    flashing icon

--- opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc/car/hyundai/hyundaicanfd.py
@@ -1,6 +1,6 @@
 import copy
 import numpy as np
-from opendbc.car import CanBusBase
+from opendbc.car import CanBusBase, DT_CTRL
 from opendbc.car.hyundai.values import HyundaiFlags
 
 
@@ -116,10 +116,26 @@ def create_acc_cancel(packer, CP, CAN, cruise_info_copy):
   })
   return packer.make_can_msg("SCC_CONTROL", CAN.ECAN, values)
 
-def create_lfahda_cluster(packer, CAN, enabled, lkasEnabled, latActive, hda_icon=0):
+lfa_icon_last = 0
+lfa_icon_frame = 0
+
+def create_lfahda_cluster(packer, CAN, enabled, lkasEnabled, latActive, hda_icon, frame):
+  global lfa_icon_last, lfa_icon_frame
+
+  lfa_icon = 2 if (enabled and latActive) else (1 if lkasEnabled else 0)
+
+  if lfa_icon_last == 2 and lfa_icon == 1:
+    lfa_icon_frame = frame
+  elif lfa_icon != 1:
+    lfa_icon_frame = 0
+
+  output_lfa_icon = 3 if (frame - lfa_icon_frame) * DT_CTRL < 1.0 else lfa_icon
+
+  lfa_icon_last = lfa_icon
+
   values = {
     "HDA_ICON": hda_icon,
-    "LFA_ICON": 2 if enabled and latActive else 1 if lkasEnabled else 0,
+    "LFA_ICON": output_lfa_icon,
   }
   return packer.make_can_msg("LFAHDA_CLUSTER", CAN.ECAN, values)
 
